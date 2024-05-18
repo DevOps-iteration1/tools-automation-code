e52711577401fd9d66c37b621a5510cc3e581aa7
Vivek <vivekanandareddy7677@gmail.com>
2024-05-18 16:45:23 -0500

    Adding policy to the role

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "tools" {
   for_each       = var.tools
   source         = "./module"
 
-  tool_name      = each.key
-  instance_type  = each.value["instance_type"]
-
-  zone_id        = var.zone_id
+  tool_name            = each.key
+  instance_type        = each.value["instance_type"]
+  policy_resource_list = each.value["policy_resource_list"]
+  zone_id              = var.zone_id
 }
