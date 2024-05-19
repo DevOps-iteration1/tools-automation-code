a72cb6038d80970c51412693e8d0029f84a205fc
Vivek <vivekanandareddy7677@gmail.com>
2024-05-19 03:17:51 -0500

    prometheus public record

--- module/main.tf
+++ module/main.tf
@@ -17,16 +17,16 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "record" {
-  name    = var.tool_name
+resource "aws_route53_record" "record_internal" {
+  name    = "${var.tool_name}_internal"
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl = 30
 }
 
-resource "aws_route53_record" "public_record" {
-  name    = "${var.tool_name}-public"
+resource "aws_route53_record" "record" {
+  name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
